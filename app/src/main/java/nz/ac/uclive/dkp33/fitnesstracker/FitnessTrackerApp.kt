8b92493bcd0a1e7296ea2777555a9abb40b509a4
Dillon Pike <dkp33@uclive.ac.nz>
2023-08-12 15:03:59 +1200

    Refactored WorkoutHistoryScreen into its own file. Added best set to workout history

--- app/src/main/java/nz/ac/uclive/dkp33/fitnesstracker/FitnessTrackerApp.kt
+++ app/src/main/java/nz/ac/uclive/dkp33/fitnesstracker/FitnessTrackerApp.kt
@@ -1,31 +1,19 @@
 package nz.ac.uclive.dkp33.fitnesstracker
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.expandIn
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.*
-import nz.ac.uclive.dkp33.fitnesstracker.model.Exercise
-import nz.ac.uclive.dkp33.fitnesstracker.model.Workout
-import nz.ac.uclive.dkp33.fitnesstracker.ui.theme.FitnessTrackerTheme
-import java.text.SimpleDateFormat
-import java.util.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun FitnessTrackerApp() {
@@ -70,104 +58,6 @@ fun HomeScreen(navController: NavController) {
 @Composable
 fun WorkoutTrackingScreen(navController: NavController) {
     Text(text = "Tracking")
-}
-
-@Composable
-fun WorkoutHistoryScreen(navController: NavController) {
-    val workoutHistory = generateDummyWorkoutHistory() // Replace with actual data
-    LazyColumn {
-        items(workoutHistory) { workout ->
-            WorkoutHistoryItem(workout = workout)
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun WorkoutHistoryItem(workout: Workout) {
-    var expanded by remember { mutableStateOf(false) }
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        elevation = 4.dp,
-        onClick = {
-            expanded = !expanded
-        }
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
-            val formattedDate = dateFormatter.format(workout.date)
-            Text(
-                text = "Workout Date: $formattedDate",
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("Exercises:")
-            Spacer(modifier = Modifier.height(4.dp))
-            Column {
-                workout.exercises.forEachIndexed { index, exercise ->
-                    ExerciseItem(exercise = exercise, showSets = expanded)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun ExerciseItem(exercise: Exercise, showSets: Boolean) {
-    val alpha = animateFloatAsState(if (showSets) 1f else 0f).value
-
-    Text(
-        text = exercise.name,
-        fontWeight = FontWeight.Bold
-    )
-    AnimatedVisibility(
-        visible = showSets,
-        enter = fadeIn() + expandVertically(),
-        exit = fadeOut() + shrinkVertically()
-    ) {
-        Column(
-            modifier = Modifier.alpha(alpha)
-        ) {
-            exercise.sets.forEachIndexed { index, (weight, repetitions) ->
-                Text(
-                    text = "  Set ${index + 1}: Weight: $weight kg, Reps: $repetitions",
-                    fontStyle = FontStyle.Italic
-                )
-            }
-        }
-    }
-    Spacer(modifier = Modifier.height(4.dp))
-}
-
-
-
-@Composable
-fun generateDummyWorkoutHistory(): List<Workout> {
-    val workoutList = mutableListOf<Workout>()
-
-    val exercise1 = Exercise("Push-up", listOf(5.0f to 15, 5.5f to 12, 6.0f to 10))
-    val exercise2 = Exercise("Squat", listOf(10.0f to 10, 11.0f to 8, 12.0f to 6))
-    val exercise3 = Exercise("Pull-up", listOf(8.0f to 8, 9.0f to 6, 10.0f to 4))
-    val exercise4 = Exercise("Plank", listOf(1.5f to 60, 1.75f to 50))
-
-    val workout1 = Workout(Date(), listOf(exercise1))
-    val workout2 = Workout(Date(), listOf(exercise1, exercise2))
-    val workout3 = Workout(Date(), listOf(exercise3, exercise4))
-    val workout4 = Workout(Date(), listOf(exercise4, exercise2))
-
-    workoutList.add(workout1)
-    workoutList.add(workout2)
-    workoutList.add(workout3)
-    workoutList.add(workout4)
-
-    // Add more workouts...
-
-    return workoutList
 }
 
 sealed class Screen(val title: String) {
---
+++ app/src/main/java/nz/ac/uclive/dkp33/fitnesstracker/WorkoutHistoryScreen.kt
@@ -0,0 +1,116 @@
+package nz.ac.uclive.dkp33.fitnesstracker
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.*
+import nz.ac.uclive.dkp33.fitnesstracker.model.Exercise
+import nz.ac.uclive.dkp33.fitnesstracker.model.Workout
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Composable
+fun WorkoutHistoryScreen(navController: NavController) {
+    val workoutHistory = generateDummyWorkoutHistory() // Replace with actual data
+    LazyColumn {
+        items(workoutHistory) { workout ->
+            WorkoutHistoryItem(workout = workout)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun WorkoutHistoryItem(workout: Workout) {
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = 4.dp,
+        onClick = {
+            expanded = !expanded
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            val dateFormatter = SimpleDateFormat("dd/MM/yyyy")
+            val formattedDate = dateFormatter.format(workout.date)
+            Text(
+                text = "Workout Date: $formattedDate",
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("Exercises:")
+            Spacer(modifier = Modifier.height(4.dp))
+            Column {
+                workout.exercises.forEachIndexed { index, exercise ->
+                    ExerciseItem(exercise = exercise, showSets = expanded)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ExerciseItem(exercise: Exercise, showSets: Boolean) {
+    val alpha = animateFloatAsState(if (showSets) 1f else 0f).value
+    val bestSet = exercise.sets.maxWith(compareBy({ it.first }, { it.second }))
+
+    Text(
+        text = "${exercise.name}  Best Set: ${bestSet.second} x ${bestSet.first} kg",
+        fontWeight = FontWeight.Bold
+    )
+    AnimatedVisibility(
+        visible = showSets,
+        enter = fadeIn() + expandVertically(),
+        exit = fadeOut() + shrinkVertically()
+    ) {
+        Column(
+            modifier = Modifier.alpha(alpha)
+        ) {
+            exercise.sets.forEachIndexed { index, (weight, repetitions) ->
+                Text(
+                    text = "  Set ${index + 1}: Weight: $weight kg, Reps: $repetitions",
+                    fontStyle = FontStyle.Italic
+                )
+            }
+        }
+    }
+    Spacer(modifier = Modifier.height(4.dp))
+}
+
+@Composable
+fun generateDummyWorkoutHistory(): List<Workout> {
+    val workoutList = mutableListOf<Workout>()
+
+    val exercise1 = Exercise("Push-up", listOf(5.0f to 15, 5.5f to 12, 6.0f to 10))
+    val exercise2 = Exercise("Squat", listOf(10.0f to 10, 11.0f to 8, 12.0f to 6))
+    val exercise3 = Exercise("Pull-up", listOf(8.0f to 8, 9.0f to 6, 10.0f to 4))
+    val exercise4 = Exercise("Plank", listOf(1.5f to 60, 1.75f to 50))
+
+    val workout1 = Workout(Date(), listOf(exercise1))
+    val workout2 = Workout(Date(), listOf(exercise1, exercise2))
+    val workout3 = Workout(Date(), listOf(exercise3, exercise4))
+    val workout4 = Workout(Date(), listOf(exercise4, exercise2))
+
+    workoutList.add(workout1)
+    workoutList.add(workout2)
+    workoutList.add(workout3)
+    workoutList.add(workout4)
+
+    // Add more workouts...
+
+    return workoutList
+}
