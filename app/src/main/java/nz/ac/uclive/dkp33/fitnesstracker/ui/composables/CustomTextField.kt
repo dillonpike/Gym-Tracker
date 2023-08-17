7797e4a83ea65f67fd8d63b38af8874ced8d7ca2
Dillon Pike <DillonPike.NZ@gmail.com>
2023-08-18 00:12:34 +1200

    UI updates

---
+++ app/src/main/java/nz/ac/uclive/dkp33/fitnesstracker/ui/composables/CustomTextField.kt
@@ -0,0 +1,82 @@
+package nz.ac.uclive.dkp33.fitnesstracker.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import nz.ac.uclive.dkp33.fitnesstracker.R
+import nz.ac.uclive.dkp33.fitnesstracker.ui.theme.LightLightGray
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    leadingIcon: (@Composable () -> Unit)? = null,
+    trailingIcon: (@Composable () -> Unit)? = null,
+    placeholderText: String = stringResource(R.string.text_field_placeholder),
+    fontSize: TextUnit = MaterialTheme.typography.body2.fontSize,
+    textAlign: TextAlign = TextAlign.Left,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    width: Dp = 80.dp
+) {
+    val backgroundColor = if (isSystemInDarkTheme()) {
+        Color.DarkGray
+    } else {
+        LightLightGray
+    }
+    BasicTextField(
+        modifier = modifier
+            .background(
+                backgroundColor,
+                MaterialTheme.shapes.small,
+            )
+            .width(width),
+        value = value,
+        onValueChange = { onValueChange(it) },
+        keyboardOptions = keyboardOptions,
+        singleLine = true,
+        cursorBrush = SolidColor(MaterialTheme.colors.primary),
+        textStyle = LocalTextStyle.current.copy(
+            color = MaterialTheme.colors.onSurface,
+            fontSize = fontSize,
+            textAlign = textAlign
+        ),
+        decorationBox = { innerTextField ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            ) {
+                if (leadingIcon != null) leadingIcon()
+                Box(Modifier.weight(1f)) {
+                    if (value.isEmpty()) Text(
+                        placeholderText,
+                        style = LocalTextStyle.current.copy(
+                            color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f),
+                            fontSize = fontSize
+                        )
+                    )
+                    innerTextField()
+                }
+                if (trailingIcon != null) trailingIcon()
+            }
+        }
+    )
+}
--- app/src/main/java/nz/ac/uclive/dkp33/fitnesstracker/ui/screens/AddWorkoutScreen.kt
+++ app/src/main/java/nz/ac/uclive/dkp33/fitnesstracker/ui/screens/AddWorkoutScreen.kt
@@ -2,11 +2,9 @@ package nz.ac.uclive.dkp33.fitnesstracker.ui.screens
 
 import android.annotation.SuppressLint
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
@@ -18,25 +16,23 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import nz.ac.uclive.dkp33.fitnesstracker.ui.AppViewModelProvider
 import nz.ac.uclive.dkp33.fitnesstracker.R
 import nz.ac.uclive.dkp33.fitnesstracker.Screen
 import nz.ac.uclive.dkp33.fitnesstracker.model.Exercise
 import nz.ac.uclive.dkp33.fitnesstracker.model.WorkoutViewModel
+import nz.ac.uclive.dkp33.fitnesstracker.ui.AppViewModelProvider
 import nz.ac.uclive.dkp33.fitnesstracker.ui.composables.BackButton
+import nz.ac.uclive.dkp33.fitnesstracker.ui.composables.CustomTextField
 import nz.ac.uclive.dkp33.fitnesstracker.ui.composables.ScreenHeading
 import nz.ac.uclive.dkp33.fitnesstracker.ui.theme.FitnessTrackerTheme
 
@@ -50,7 +46,9 @@ fun WorkoutTrackingScreen(navController: NavController, workoutViewModel: Workou
         topBar = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
             ) {
                 BackButton(navController)
                 ScreenHeading(text = stringResource(R.string.add_workout_heading))
@@ -111,7 +109,9 @@ private fun AddExerciseButton(workoutViewModel: WorkoutViewModel) {
         onClick = {
             workoutViewModel.addExercise()
         },
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp, horizontal=16.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
     ) {
         Icon(
             imageVector = Icons.Default.Add,
@@ -148,19 +148,24 @@ private fun NameTextField(value : String, onValueChange: (String) -> Unit) {
         onValueChange = { onValueChange(it) },
         fontSize = 16.sp,
         placeholderText = stringResource(R.string.text_field_name_placeholder),
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier.padding(8.dp),
+        width = 1000.dp
     )
 }
 
 @Composable
 private fun SetTextField(value: String, onValueChange: (String) -> Unit) {
-    CustomTextField(
-        value = value,
-        onValueChange = { onValueChange(it) },
-        fontSize = 16.sp,
-        textAlign = TextAlign.Center,
-        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
-    )
+    Box(
+        modifier = Modifier.padding(2.dp)
+    ) {
+        CustomTextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+        )
+    }
 }
 
 @Composable
@@ -228,56 +233,6 @@ private fun SetRows(workoutViewModel: WorkoutViewModel, exercise: Exercise, exer
             }
         }
     }
-}
-
-@Composable
-private fun CustomTextField(
-    value: String,
-    onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-    leadingIcon: (@Composable () -> Unit)? = null,
-    trailingIcon: (@Composable () -> Unit)? = null,
-    placeholderText: String = stringResource(R.string.text_field_placeholder),
-    fontSize: TextUnit = MaterialTheme.typography.body2.fontSize,
-    textAlign: TextAlign = TextAlign.Left,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
-) {
-    BasicTextField(
-//        modifier = modifier
-//        .background(
-//            MaterialTheme.colors.surface,
-//            MaterialTheme.shapes.small,
-//        ),
-        value = value,
-        onValueChange = { onValueChange(it) },
-        keyboardOptions = keyboardOptions,
-        singleLine = true,
-        cursorBrush = SolidColor(colors.primary),
-        textStyle = LocalTextStyle.current.copy(
-            color = colors.onSurface,
-            fontSize = fontSize,
-            textAlign = textAlign
-        ),
-        decorationBox = { innerTextField ->
-            Row(
-                modifier,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (leadingIcon != null) leadingIcon()
-                Box(Modifier.weight(1f)) {
-                    if (value.isEmpty()) Text(
-                        placeholderText,
-                        style = LocalTextStyle.current.copy(
-                            color = colors.onSurface.copy(alpha = 0.3f),
-                            fontSize = fontSize
-                        )
-                    )
-                    innerTextField()
-                }
-                if (trailingIcon != null) trailingIcon()
-            }
-        }
-    )
 }
 
 @Preview(showBackground = true)
